f82fec59507718375ed5acc9675181a42ca4de69
itstrH <trannguyenbahuy2908@gmail.com>
2025-04-16 11:12:19 +0700

    update api get, post ticker booking

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const mysql = require('mysql2');
 const cors = require('cors');
-require('dotenv').config();
 
 const app = express();
 const port = 3001;
@@ -54,14 +53,14 @@ app.get('/api/events/:eventId', (req, res) => {
       if (results.length === 0) {
         res.status(404).json({ error: 'Không tìm thấy sự kiện' });
       } else {
-        res.json(results[0]); // Trả về object sự kiện, không phải mảng
+        res.json(results[0]);
       }
     }
   });
 });
 
 
-// API lấy danh sách vé theo eventId
+// api lấy danh sách vé theo eventId
 app.get('/api/tickets', (req, res) => {
   const { eventId } = req.query;
   if (!eventId) {
@@ -70,12 +69,58 @@ app.get('/api/tickets', (req, res) => {
 
   db.query('SELECT ticket_id, event_id, price_vnd, quantity FROM tickets WHERE event_id = ?', [eventId], (err, results) => {
     if (err) {
-      console.error('Lỗi truy vấn tickets:', err);
-      return res.status(500).json({ error: 'Lỗi server khi truy vấn tickets' });
+      console.error('Lỗi truy vấn', err);
+      return res.status(500).json({ error: 'Lỗi server' });
     }
     res.json(results);
   });
 });
+
+
+// api tạo booking mới (không cần user)
+app.post("/api/bookings", (req, res) => {
+
+  const { ticket_id, quantity, booking_date } = req.body;
+  if (!ticket_id || !quantity || !booking_date) {
+    return res.status(400).json({ error: "Thiếu thông tin đặt vé" });
+  }
+  const booking_id = `booking_${Date.now()}`;
+
+  // Chèn vào bảng bookings
+  db.query(
+    "INSERT INTO bookings (booking_id, ticket_id, booking_date, quantity) VALUES (?, ?, ?, ?)",
+    [booking_id, ticket_id, booking_date, quantity],
+    (err) => {
+      if (err) {
+        console.error("Lỗi khi thêm booking:", err);
+        return res.status(500).json({ error: "Lỗi khi đặt vé" });
+      }
+      return res.status(200).json({ message: "Đặt vé thành công" });
+    }
+  );
+});
+
+// API lấy tất cả vé đã đặt
+app.get("/api/bookings", (req, res) => {
+  db.query(
+    `SELECT b.booking_id, b.quantity, b.booking_date, e.event_name, e.event_date, e.event_location
+     FROM bookings b
+     JOIN tickets t ON b.ticket_id = t.ticket_id
+     JOIN events e ON t.event_id = e.event_id
+     ORDER BY b.booking_date DESC`,
+    (err, results) => {
+      if (err) {
+        console.error("Lỗi khi lấy danh sách vé:", err);
+        return res.status(500).json({ error: "Lỗi server" });
+      }
+      return res.json(results);
+    }
+  );
+});
+
+
+
+
 
 
 
